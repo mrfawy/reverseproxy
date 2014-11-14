8952499ab9725217f97dd817e860b11a4a02c99e
mrfawy <mrfawy@yahoo.com>
2014-11-14 16:01:15 -0600

    use as object , keep skipped/modified list

--- cnsl.py
+++ cnsl.py
@@ -5,19 +5,6 @@ import re
 import argparse
 
 OS_IS_WIN=False
-SkippedRecords=[]
-ModifiedFilesList=[]
-currentProcessedFilePath=""
-currentProcessedLineNo=""
-currentProcessedLineTxt=""
-currentProcessedRecord=None
-
-class Record:
-    def __init__(self,file_path="",line_number="",line=""):
-        self.file_name=file_name
-        self.file_path=file_path
-        self.line_number=line_number
-        self.line=line
 
 class bcolors:
     HEADER = '\033[95m'
@@ -27,6 +14,31 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
+class Record:
+    def __init__(self,file_path="",line_number="",line=""):
+        self.file_path=file_path
+        self.line_number=line_number
+        self.line=line
+
+class FileProcessor:
+
+    def __init__(self,filepaths,patterns):
+        self.filepaths=filepaths
+        self.patterns=patterns
+        self.skippedRecords=[]
+        self.modifiedRecrods=[]
+        self.currentProcessedFilePath=""
+        self.currentProcessedLineNo=""
+        self.currentProcessedLineTxt=""
+        self.currentProcessedRecord=None
+
+
+
+    def getCurrentProcessedRecord(self,skipFileDetails=False):
+        if skipFileDetails:
+            return Record(self.currentProcessedFilePath,"ALL","ALL")
+        return Record(self.currentProcessedFilePath,self.currentProcessedLineNo,self.currentProcessedLineTxt)
+
 # def rlinput(prompt, prefill=''):
 #    readline.set_startup_hook(lambda: readline.insert_text(prefill))
 #    try:
@@ -34,134 +46,174 @@ class bcolors:
 #    finally:
 #       readline.set_startup_hook()
 
-def read_acceptance():
-    while True:
-        choice=input("Do you accept this modification?[Y|S]")
-        if choice.lower()=="y":
-            return True
-        if choice.lower()=="s":
-            return False
+    def read_acceptance(self):
+        while True:
+            choice=input("Do you accept this modification?[Y|S]")
+            if choice.lower()=="y":
+                return True
+            if choice.lower()=="s":
+                return False
 
-def prompt_line_Change(pre_line,post_lines):
-    print("Current Matching Line :\n "+pre_line)
-    print("Suggested Modification:\n")
-    for post_line in post_lines:
-        print (post_line)
-    accept=read_acceptance()
-    if not accept:
-        post_lines=pre_line
-    #while not accept:
-          #post_line=rlinput("Please Edit :\n",post_lines)
-          #accept=read_acceptance()
-    return post_lines
+    def prompt_line_Change(self,pre_line,post_lines):
+        print("Current Matching Line :\n "+pre_line)
+        print("Suggested Modification:\n")
+        for post_line in post_lines:
+            print (post_line)
+        accept=self.read_acceptance()
+        if not accept:
+            self.skippedRecords.append(self.getCurrentProcessedRecord())
+            post_lines=pre_line
+        #while not accept:
+              #post_line=rlinput("Please Edit :\n",post_lines)
+              #accept=read_acceptance()
+        self.modifiedRecrods.append(self.getCurrentProcessedRecord())
+        return post_lines
 
-def get_buffered_lines_indexes(lines,pos,window=5):
-    if pos<0 or pos>len(lines):
-        return null
+    def get_buffered_lines_indexes(self,lines,pos,window=5):
+        if pos<0 or pos>len(lines):
+            return null
 
-    start_index=pos-((window-1)/2)
-    if start_index<0:
-       start_index=0
-    end_index=pos+((window-1)/2)
-    if end_index>len(lines):
-       end_index=len(lines)
-    return int(start_index),int(end_index)
+        start_index=pos-((window-1)/2)
+        if start_index<0:
+           start_index=0
+        end_index=pos+((window-1)/2)
+        if end_index>len(lines):
+           end_index=len(lines)
+        return int(start_index),int(end_index)
 
-def draw_file_contents(filepath,lines,pos):
-    if OS_IS_WIN:
-        os.system('cls')
-    else:
-        os.system('clear')
-    print("File Contents:"+filepath)
-    print("---------------")
-    start_index,end_index=get_buffered_lines_indexes(lines,pos,5)
-    for index in range(start_index,end_index):
-        if index==pos:
-           print (bcolors.OKBLUE+lines[index]+bcolors.ENDC)
-        else:
-            print(lines[index])
-    print("=======================================================================================================================")
-
-def is_matching_line(line,patterns):
-    for pattern in patterns :
-        if  re.search(pattern, line.lower(),re.IGNORECASE):
-            return pattern
-    return None
-
-def get_suggested_line(line,matched_pattern,count=1):
-
-    startIndex=line.find(matched_pattern)
-    if(startIndex!=-1):
-        endIndex=line.find('"',startIndex)
-        leadingSpacesNo=len(line) - len(line.lstrip())
+    def draw_file_contents(self,filepath,lines,pos):
         if OS_IS_WIN:
-            leadingSpacesTxt=" ".join(" " for i in range(0,leadingSpacesNo))
+            os.system('cls')
         else:
-            leadingSpacesTxt=" ".join("\t" for i in range(0,leadingSpacesNo))
-        convTemplate="""<cfinvoke component="NBP.NetBiosProxyHelper" method="getTemplatePathForACWithEncryptedValue" fileName="{1}" returnvariable="{0}" />"""
-        replaceToken="#encryptedValue"+str(count)+"#"
-        textToBeReplaced=line[startIndex:endIndex]
-        convLine=convTemplate.format(replaceToken,textToBeReplaced)
-        new_line=line[0:startIndex]+replaceToken+line[endIndex:len(line)]
-        convLine=leadingSpacesTxt+convLine
-        return [convLine,leadingSpacesTxt+"<cfoutput>",new_line,leadingSpacesTxt+"</cfoutput>"]
-    else:
-        print ("invalid processing ")
-        return ["ERROR!! : unable to retreive this line suggesstions!!"]
+            os.system('clear')
+        print("File Contents:"+filepath)
+        print("---------------")
+        start_index,end_index=self.get_buffered_lines_indexes(lines,pos,5)
+        for index in range(start_index,end_index):
+            if index==pos:
+               print (bcolors.OKBLUE+lines[index]+bcolors.ENDC)
+            else:
+                print(lines[index])
+        print("=======================================================================================================================")
 
-def is_file_contains_Patterns(patterns,filepath):
-    lines=[]
-    try:
-        lines=open(filepath,"r").readlines()
-    except:
-        print("unable to read/parse file "+filepath)
+    def is_matching_line(self,line,patterns):
+        for pattern in patterns :
+            if  re.search(pattern, line.lower(),re.IGNORECASE):
+                return pattern
+        return None
+
+    def get_suggested_line(self,line,matched_pattern,count=1):
+
+        startIndex=line.find(matched_pattern)
+        if(startIndex!=-1):
+            endIndex=line.find('"',startIndex)
+            leadingSpacesNo=len(line) - len(line.lstrip())
+            if OS_IS_WIN:
+                leadingSpacesTxt=" ".join(" " for i in range(0,leadingSpacesNo))
+            else:
+                leadingSpacesTxt=" ".join("\t" for i in range(0,leadingSpacesNo))
+            convTemplate="""<cfinvoke component="NBP.NetBiosProxyHelper" method="getTemplatePathForACWithEncryptedValue" fileName="{1}" returnvariable="{0}" />"""
+            replaceToken="#encryptedValue"+str(count)+"#"
+            textToBeReplaced=line[startIndex:endIndex]
+            convLine=convTemplate.format(replaceToken,textToBeReplaced)
+            new_line=line[0:startIndex]+replaceToken+line[endIndex:len(line)]
+            convLine=leadingSpacesTxt+convLine
+            return [convLine,leadingSpacesTxt+"<cfoutput>",new_line,leadingSpacesTxt+"</cfoutput>"]
+        else:
+            print ("invalid processing ")
+            return ["ERROR!! : unable to retreive this line suggesstions!!"]
+
+    def is_file_contains_Patterns(self,patterns,filepath):
+        lines=[]
+        try:
+            lines=open(filepath,"r").readlines()
+        except:
+            print("unable to read/parse file "+filepath)
+            return False
+        for line in lines:
+            if self.is_matching_line(line,patterns):
+                return True
         return False
-    for line in lines:
-        if is_matching_line(line,patterns):
+
+    def prompt_file_Change(self,filepath):
+        print("File:"+filepath)
+        print("is matching with one or more patterns, it'll be edited")
+        accept=self.read_acceptance()
+        if accept:
             return True
-    return False
+        return False
 
-def prompt_file_Change(filepath):
-    print("File:"+filepath)
-    print("is matching with one or more patterns, it'll be edited")
-    accept=read_acceptance()
-    if accept:
-        return True
-    return False
-
-def process_file(patterns,filepath):
-    print ("Scanning File :"+filepath)
-    isMatchingFile=is_file_contains_Patterns(patterns,filepath)
-    if not isMatchingFile:
-        return
-    else:
-        if not prompt_file_Change(filepath):
+    def process_file(self,patterns,filepath):
+        print ("Scanning File :"+filepath)
+        self.currentProcessedFilePath=filepath
+        isMatchingFile=self.is_file_contains_Patterns(self.patterns,filepath)
+        if not isMatchingFile:
             return
-    #start editing file
-    old_lines=open(filepath,"r").readlines()
-    new_lines=[]
-    count=1 #counter for current changes in file
-    for pos in range(len(old_lines)):
-        line=old_lines[pos]
-        matched_pattern=is_matching_line(line,patterns)
-        if matched_pattern :
-            draw_file_contents(filepath,old_lines,pos)
-            pre_line=line
-            post_lines=get_suggested_line(pre_line,matched_pattern,count)
-            post_lines=prompt_line_Change(pre_line,post_lines)
-            new_lines.extend(post_lines)
-            count+=1
         else:
-         new_lines.append(line+"\n")
-    output=open(filepath,"w")
-    output.write("\n".join(new_lines))
+            if not self.prompt_file_Change(filepath):
+                self.skippedRecords.append(self.getCurrentProcessedRecord(True))
+                return
+        #start editing file
+        old_lines=open(filepath,"r").readlines()
+        new_lines=[]
+        count=1 #counter for current changes in file
+        for pos in range(len(old_lines)):
+            line=old_lines[pos]
+            self.currentProcessedLineNo=pos
+            self.currentProcessedLineTxt=line
+            matched_pattern=self.is_matching_line(line,self.patterns)
+            if matched_pattern :
+                self.draw_file_contents(filepath,old_lines,pos)
+                pre_line=line
+                post_lines=self.get_suggested_line(pre_line,matched_pattern,count)
+                post_lines=self.prompt_line_Change(pre_line,post_lines)
+                new_lines.extend(post_lines)
+                count+=1
+            else:
+             new_lines.append(line+"\n")
+        output=open(filepath,"w")
+        output.write("\n".join(new_lines))
 
 
-def process_Files(patterns,full_file_paths):
-    print ("Starting Python processing")
-    for filepath in full_file_paths:
-        process_file(patterns,filepath)
+    def process_Files(self):
+        print ("Starting Python processing")
+        for filepath in self.filepaths:
+            self.process_file(self.patterns,filepath)
 
+
+    def printSkippedRecords(self):
+        print ("Skipped Lines :")
+        for record in self.skippedRecords:
+            print ( record.file_path +"||"+str(record.line_number)+"||"+record.line[:100] )
+
+    def printModifiedRecords(self):
+        print ("Modified Lines :")
+        for record in self.modifiedRecrods:
+            print ( record.file_path +"||"+str(record.line_number)+"||"+record.line[:100] )
+
+
+#To retreive encrypted value via Restful call
+    def getEncryptedUrl(self,url):
+        url=""
+        params = urllib.urlencode({'@url': url})
+        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection(url)
+        conn.request("POST", "", params, headers)
+        response = conn.getresponse()
+        print (response.status)
+        data = response.read()
+
+# Start of the Script
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="directory path to start scanning ")
+parser.add_argument("-p","--pattern",nargs = '*' , help="list of pattern(s) to match ,please refer to Python Regex documentation .Default:'/download/' '/manuals' '/HelpDoc' '/reports/'",action="append",default=[["/download/","/manuals","/HelpDoc","/reports/"]])
+
+args = parser.parse_args()
+DIR_PATH=args.path
+PATTERNS=args.pattern
+if "nt" in os.name:
+    print ("Detected Windows like OS")
+    OS_IS_WIN=True
 
 def get_filepaths(directory,includePatterns,skip_patterns):
 
@@ -182,31 +234,11 @@ def get_filepaths(directory,includePatterns,skip_patterns):
                         file_paths.append(filepath)
     return file_paths
 
-
-#To retreive encrypted value via Restful call
-def getEncryptedUrl(url):
-    url=""
-    params = urllib.urlencode({'@url': url})
-    headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-    conn = httplib.HTTPConnection(url)
-    conn.request("POST", "", params, headers)
-    response = conn.getresponse()
-    print (response.status)
-    data = response.read()
-
-# Start of the Script
-parser = argparse.ArgumentParser()
-parser.add_argument("path", help="directory path to start scanning ")
-parser.add_argument("-p","--pattern",nargs = '*' , help="list of pattern(s) to match ,please refer to Python Regex documentation .Default:'/download/' '/manuals' '/HelpDoc' '/reports/'",action="append",default=[["/download/","/manuals","/HelpDoc","/reports/"]])
-
-args = parser.parse_args()
-DIR_PATH=args.path
-PATTERNS=args.pattern
-if "nt" in os.name:
-    print ("Detected Windows like OS")
-    OS_IS_WIN=True
 full_file_paths = get_filepaths(DIR_PATH,[".cfm","cfc"],[".svn",".git"])
-process_Files(PATTERNS[-1],full_file_paths) # -1  to always get the last , as if parm is submitted it's appended
+processor=FileProcessor(full_file_paths,PATTERNS[-1])  # -1  to always get the last , as if parm is submitted it's appended
+processor.process_Files()
+processor.printSkippedRecords()
+processor.printModifiedRecords()
 print ("DONE")
 
 
